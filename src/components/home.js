3d5ff0bf9db215a8dd5d32a0b52907bdfaa08f18
Vinod Tomy <vinod@teamevoque.com>
2019-03-06 11:59:02 +0530

    testing media query

--- src/components/home.js
+++ src/components/home.js
@@ -68,30 +68,32 @@ class Home extends Component {
         <section className="e-p-top-100 e-p-bottom-50">
           <div className="container e-relative">
             
-            <div className="row">
-              <div className="col-7">
-                <h2 className="e-p-bottom-20">Brilliant Ideas for <br/><span className="e-c-grey">Zero Fee</span></h2>
-                <h5 className="e-c-brown">We help you to build a data-driven, diversified, low cost & long term successful portfolio based on the trend in the economy for free!</h5>
-              </div>
-              <div className="col-5 e-bg-white e-ftheme-card-wrap e-hm-ftheme-card-wrap e-relative">
-                
-                <div className="row e-hm-topTheme-wrap e-show">
-                    <div className="col-8">
-                      <h4>Energy takeout targets</h4>
-                      <p className="e-card-p-txt e-c-brown">This is to gain exposure to mergers & amo acquisitions activity among oil & gas companies.</p>
-                    </div>
-                    <div className="col-4 e-align-center ">
-                      <img className="e-ftheme-card-img e-m-bottom-10 e-full-width" src={ideaImage1} alt="Themes by Fyers"/>
-                    </div>
+            <div className="row ">
+              <div className="col-sm-12 d-flex flex-row m-flex-column ">
+                <div className="col-md-7 col-sm-12 e-p-left-0 em-p-bottom">
+                  <h2 className="e-p-bottom-20">Brilliant Ideas for <br/><span className="e-c-grey">Zero Fee</span></h2>
+                  <h5 className="e-c-brown">We help you to build a data-driven, diversified, low cost & long term successful portfolio based on the trend in the economy for free!</h5>
                 </div>
-                <div className="row">
-                  <div className="col-8">
-                    <div className="d-flex ">
-                      <p className="e-card-pl-txt e-p-0 e-m-0 e-c-brown mt-auto p-2">MONTHLY RETURNS <span className="e-c-green">5.6%</span></p>
-                    </div>
+                <div className="col-md-5 col-sm-12 e-bg-white e-ftheme-card-wrap e-hm-ftheme-card-wrap e-relative ">
+                  
+                  <div className="row e-hm-topTheme-wrap e-show">
+                      <div className="col-8">
+                        <h4>Energy takeout targets</h4>
+                        <p className="e-card-p-txt e-c-brown">This is to gain exposure to mergers & amo acquisitions activity among oil & gas companies.</p>
+                      </div>
+                      <div className="col-4 e-align-center ">
+                        <img className="e-ftheme-card-img e-m-bottom-10 e-full-width" src={ideaImage1} alt="Themes by Fyers"/>
+                      </div>
                   </div>
-                  <div className="col-4 e-align-center">
-                    <a href="/theme-detail/overview" className="e-a-primary-c" aria-label="Invest now">Invest Now</a>
+                  <div className="row">
+                    <div className="col-8">
+                      <div className="d-flex ">
+                        <p className="e-card-pl-txt e-p-0 e-m-0 e-c-brown mt-auto p-2">MONTHLY RETURNS <span className="e-c-green">5.6%</span></p>
+                      </div>
+                    </div>
+                    <div className="col-4 e-align-center">
+                      <a href="/theme-detail/overview" className="e-a-primary-c" aria-label="Invest now">Invest Now</a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -112,26 +114,26 @@ class Home extends Component {
           <img className="e-hw-to-pattern" alt="How to work pattern"/>
           <div className="container">
             <div className="row">
-              <div className="col-6 e-p-bottom-30">
+              <div className="col-md-6 col-sm-12 e-p-bottom-30">
                 <h2 className="e-p-bottom-20 e-c-white">How to start <br/><span className="e-c-dull-secondary">Thematic?</span></h2>
                 <h5 className="e-c-brown e-hm-how-head-sec">With 3 simple steps you can begin your bright investment future with us</h5>
               </div>
             </div>
             <div className="col-11 e-p-top-50 e-hm-how-items-wrap">
               <div className="row">
-                <div className="col-3 offset-md-1 e-hm-how-pick-wrap">
+                <div className="col-md-3 offset-md-1 e-hm-how-pick-wrap e-m-p-bottom-30">
                   <img className="e-p-bottom-10" alt="How to work pick"/>
                   <h3 className="e-c-white e-p-bottom-10">Pick</h3>
                   <p className="e-hm-how-para e-p-bottom-10">Choose an existing theme from our inventory to start investing, evaluating or follow it.</p>
                   <a aria-label="View Themes" className="e-a-bg-secondary-c" href="/">VIEW THEMES</a>
                 </div>
-                <div className="col-3 offset-md-1 e-hm-how-customize-wrap">
+                <div className="col-md-3 offset-md-1 e-hm-how-customize-wrap e-m-p-bottom-30">
                   <img className="e-p-bottom-10" alt="How to work pick"/>
                   <h3 className="e-c-white e-p-bottom-10">Pick</h3>
                   <p className="e-hm-how-para e-p-bottom-10">If you want to add or remove any instrument to or from the chosen theme, you can do that.</p>
                   <a aria-label="View Themes" className="e-a-bg-secondary-c" href="/">VIEW THEMES</a>
                 </div>
-                <div className="col-3 offset-md-1 e-hm-how-invest-wrap">
+                <div className="col-md-3 offset-md-1 e-hm-how-invest-wrap e-m-p-bottom-30">
                   <img className="e-p-bottom-10" alt="How to work pick"/>
                   <h3 className="e-c-white e-p-bottom-10">Invest</h3>
                   <p className="e-hm-how-para e-p-bottom-10">If you believe the chosen theme or your customised idea can perform, start buying it.</p>
@@ -143,86 +145,86 @@ class Home extends Component {
         </section>
         <section className="e-p-top-100 e-p-bottom-100">
           <div className="container">
-            <div className="col-6 e-p-bottom-30">
+            <div className="col-md-6 e-p-bottom-30">
               <h2 className="e-p-bottom-20 e-c-black">Why investment in <br/><span className="e-c-grey">Ideas?</span></h2>
               <h5 className="e-c-black">Investing in multiple stocks can reduce risk and can beat other investment techniques, also</h5>
             </div>
-            <div className="col-11 e-p-top-50 e-hm-why-items-wrap">
+            <div className="col-md-11 e-p-top-50 e-hm-why-items-wrap">
               <div className="row">
-                <div className="col-3 offset-md-1">
+                <div className="col-md-3 offset-md-1 e-m-p-bottom-30">
                   <div className="row e-p-bottom-50 e-hm-why-easy-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4 ">
                       <img alt="Easy & simple method to invest"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Easy & simple method to invest</p>
                     </div>
                   </div>
                   <div className="row e-p-bottom-50 e-hm-why-customizable-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="Highly customizable to perform"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Highly customizable to perform</p>
                     </div>
                   </div>
                   <div className="row e-hm-why-uPortfolio-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="100+ unique portfolios to follow"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">100+ unique portfolios to follow</p>
                     </div>
                   </div>
                 </div>
-                <div className="col-3 offset-md-1">
+                <div className="col-md-3  offset-md-1 e-m-p-bottom-30">
                   <div className="row e-p-bottom-50 e-hm-why-researched-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="Gets thoroughly researched portfolios"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Gets thoroughly researched portfolios</p>
                     </div>
                   </div>
                   <div className="row e-p-bottom-50 e-hm-why-fee-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="No management fee,no hidden costs"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">No management fee,no hidden costs</p>
                     </div>
                   </div>
                   <div className="row e-hm-why-ideas-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="Invest in your own ideas you believe"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Invest in your own ideas you believe</p>
                     </div>
                   </div>
                 </div>
-                <div className="col-3 offset-md-1">
+                <div className="col-md-3  offset-md-1">
                   <div className="row e-p-bottom-50 e-hm-why-mfunds-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="Outperform mutual funds in every way"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Outperform mutual funds in every way</p>
                     </div>
                   </div>
                   <div className="row e-p-bottom-50 e-hm-why-fcontrol-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="Full control over your portfolio"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">Full control over your portfolio</p>
                     </div>
                   </div>
                   <div className="row e-hm-why-more-wrap">
-                    <div className="col-4">
+                    <div className="col-md-4">
                       <img alt="And a lot more"/>
                     </div>
-                    <div className="col-8 e-p-right-0">
+                    <div className="col-md-8 e-p-right-0">
                       <p className="e-c-black">And a lot more</p>
                     </div>
                   </div>
@@ -239,14 +241,14 @@ class Home extends Component {
           </div>
           <div className="container">
             <div className="row">
-              <div className="col-7">
+              <div className="col-7 col-sm-7">
                 <h5 className="e-c-black">Themes from Fyers which outperform the mutual funds</h5>
               </div>
               <img className="e-top-theme-pattern-img-1" alt="Fyers pattern"/>
             </div>
-            <div className="col-12 e-p-top-50 e-ptheme-items-wrap e-p-left-0">
+            <div className="col-md-12 col-sm-12 e-p-top-50 e-ptheme-items-wrap e-p-left-0">
               <div className="row">
-                <div className="col-3 ">
+                <div className="col-md-3 col-sm-4">
                   <div className="e-ptheme-card-wrap e-card-hover">
                     <img className="e-m-bottom-10 e-border-5" src={publicTheme1} alt="Themes by Fyers"/>
                     <h4>Energy takeout targets</h4>
@@ -257,7 +259,7 @@ class Home extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="col-3">
+                <div className="col-md-3 col-sm-4">
                   <div className="e-ptheme-card-wrap e-card-hover">
                     <img className="e-m-bottom-10 e-border-5" src={publicTheme1} alt="Themes by Fyers"/>
                     <h4>Energy takeout targets</h4>
@@ -268,7 +270,7 @@ class Home extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="col-3">
+                <div className="col-md-3 col-sm-4">
                   <div className="e-ptheme-card-wrap e-card-hover">
                     <img className="e-m-bottom-10 e-border-5" src={publicTheme1} alt="Themes by Fyers"/>
                     <h4>Energy takeout targets</h4>
@@ -279,7 +281,7 @@ class Home extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="col-3">
+                <div className="col-md-3 col-sm-4">
                   <div className="e-ptheme-card-wrap e-card-hover">
                     <img className="e-m-bottom-10 e-border-5" src={publicTheme1} alt="Themes by Fyers"/>
                     <h4>Energy takeout targets</h4>
